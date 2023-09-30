4caf0f547dab5da33bf17c20598ccbfbbec31a1f
Abdirahman Jama <mustalid21@gmail.com>
2023-09-29 23:43:46 -0500

    added query to get all genres

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool')
+const pool = require("../modules/pool");
 
-router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+router.get("/", (req, res) => {
+  const query = `SELECT * FROM genres`;
+  pool
+    .query(query)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("ERROR: Get all genres", err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
